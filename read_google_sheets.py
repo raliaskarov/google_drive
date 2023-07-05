de429db6d353f0528a7e1c41d09c28602b2f9619
raliaskarov <61615456+raliaskarov@users.noreply.github.com>
2023-07-05 17:01:00 +0200

    Update read_google_sheets.py

--- read_google_sheets.py
+++ read_google_sheets.py
@@ -6,43 +6,48 @@ import pickle
 
 
 # ID for google sheets to access
-sheet_id = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" # <-- your ID here
- 
-# If modifying these SCOPES, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+workbook_id = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+path_to_secrets = "path/to/your/credentials/file.json"
 
-creds = None
-# The file token.pickle stores the user's access and refresh tokens, and is
-# created automatically when the authorization flow completes for the first time.
-if os.path.exists('token.pickle'):
-    with open('token.pickle', 'rb') as token:
-        creds = pickle.load(token)
+def read_google_sheets(workbook_id, path_to_secrets):
+    # If modifying these SCOPES, delete the file token.pickle.
+    SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
-# If there are no (valid) credentials available, let the user log in.
-if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-        creds.refresh(Request())
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                path_to_secrets, SCOPES)
+            creds = flow.run_local_server(port=0)
+
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('sheets', 'v4', credentials=creds)
+
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+
+    # Replace 'SPREADSHEET_ID' with your specific Spreadsheet ID
+    result = sheet.values().get(spreadsheetId = workbook_id,
+                                range='data').execute()
+    values = result.get('values', [])
+
+    if not values:
+        print('No data found.')
     else:
-        flow = InstalledAppFlow.from_client_secrets_file(
-            'credentials.json', SCOPES)                        # <-- path to secrets here
-        creds = flow.run_local_server(port=0)
+        df = pd.DataFrame(values[1:], columns=values[0])
+    
+    return df
 
-    # Save the credentials for the next run
-    with open('token.pickle', 'wb') as token:
-        pickle.dump(creds, token)
-
-service = build('sheets', 'v4', credentials=creds)
-
-# Call the Sheets API
-sheet = service.spreadsheets()
-
-# Replace 'SPREADSHEET_ID' with your specific Spreadsheet ID
-result = sheet.values().get(spreadsheetId = sheet_id,
-                            range='data').execute()
-values = result.get('values', [])
-
-if not values:
-    print('No data found.')
-else:
-    for row in values:
-        print(row)
+read_google_sheets(workbook_id, path_to_secrets)
